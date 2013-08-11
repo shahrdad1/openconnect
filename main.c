e3f4e30798699deb08a05336822727c1f9e88860
Kevin Cernekee <cernekee@gmail.com>
2013-08-10 18:34:40 -0700

    main: Add openconnect_vpninfo_free() on a couple of exit paths
    This makes it easier to check for library memory leaks.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- main.c
+++ main.c
@@ -894,6 +894,7 @@ int main(int argc, char **argv)
 	}
 	if (openconnect_make_cstp_connection(vpninfo)) {
 		fprintf(stderr, _("Creating SSL connection failed\n"));
+		openconnect_vpninfo_free(vpninfo);
 		exit(1);
 	}
 
@@ -902,10 +903,12 @@ int main(int argc, char **argv)
 	if (script_tun) {
 		if (openconnect_setup_tun_script(vpninfo, vpnc_script)) {
 			fprintf(stderr, _("Set up tun script failed\n"));
+			openconnect_vpninfo_free(vpninfo);
 			exit(1);
 		}
 	} else if (openconnect_setup_tun_device(vpninfo, vpnc_script, ifname)) {
 		fprintf(stderr, _("Set up tun device failed\n"));
+		openconnect_vpninfo_free(vpninfo);
 		exit(1);
 	}
 
@@ -913,6 +916,7 @@ int main(int argc, char **argv)
 		if (setuid(uid)) {
 			fprintf(stderr, _("Failed to set uid %ld\n"),
 				(long)uid);
+			openconnect_vpninfo_free(vpninfo);
 			exit(1);
 		}
 	}
@@ -948,6 +952,7 @@ int main(int argc, char **argv)
 			if (!fp) {
 				fprintf(stderr, _("Failed to open '%s' for write: %s\n"),
 					pidfile, strerror(errno));
+				openconnect_vpninfo_free(vpninfo);
 				exit(1);
 			}
 		}
@@ -959,6 +964,7 @@ int main(int argc, char **argv)
 			vpn_progress(vpninfo, PRG_INFO,
 				     _("Continuing in background; pid %d\n"),
 				     pid);
+			openconnect_vpninfo_free(vpninfo);
 			exit(0);
 		}
 		if (fp)
@@ -971,6 +977,7 @@ int main(int argc, char **argv)
 
 	if (fp)
 		unlink(pidfile);
+	openconnect_vpninfo_free(vpninfo);
 	exit(1);
 }
 
