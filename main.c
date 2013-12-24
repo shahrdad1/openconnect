c6491e86badfb40b28823ba738bce65bf7617a26
Kevin Cernekee <cernekee@gmail.com>
2013-12-24 11:44:18 -0800

    main: Avoid infinite "303 See Other" loops on certificate-only authgroups
    Since we are now caching the authgroup selection, it is possible to
    receive a form from the server that skips all user prompts.  If the
    selected authgroup is set up for certificate auth only (no l/p) and the
    client certificate is not valid, the server will render a new form which
    shows only the authgroup dropdown.  Since our CLI caches this response,
    we need to make sure it doesn't keep resubmitting the same response over
    and over again.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- main.c
+++ main.c
@@ -87,6 +87,7 @@ char *username;
 char *password;
 char *authgroup;
 int authgroup_set;
+int last_form_empty;
 
 enum {
 	OPT_AUTHENTICATE = 0x100,
@@ -666,7 +667,8 @@ int main(int argc, char **argv)
 			vpninfo->script_tun = 1;
 			break;
 		case 'u':
-			username = keep_config_arg();
+			free(username);
+			username = strdup(config_arg);
 			break;
 		case 'U': {
 			char *strend;
@@ -1203,6 +1205,7 @@ static int process_auth_form_cb(void *_vpninfo,
 {
 	struct openconnect_info *vpninfo = _vpninfo;
 	struct oc_form_opt *opt;
+	int empty = 1;
 
 	if (form->banner && verbose > PRG_ERR)
 		fprintf(stderr, "%s\n", form->banner);
@@ -1241,17 +1244,20 @@ static int process_auth_form_cb(void *_vpninfo,
 				continue;
 			if (prompt_opt_select(vpninfo, select_opt, NULL) < 0)
 				goto err;
+			empty = 0;
 
 		} else if (opt->type == OC_FORM_OPT_TEXT) {
 			if (username &&
 			    !strcmp(opt->name, "username")) {
-				opt->value = strdup(username);
+				opt->value = username;
+				username = NULL;
 			} else {
 				opt->value = prompt_for_input(opt->label, vpninfo, 0);
 			}
 
 			if (!opt->value)
 				goto err;
+			empty = 0;
 
 		} else if (opt->type == OC_FORM_OPT_PASSWORD) {
 			if (password &&
@@ -1264,6 +1270,7 @@ static int process_auth_form_cb(void *_vpninfo,
 
 			if (!opt->value)
 				goto err;
+			empty = 0;
 		}
 	}
 
@@ -1271,6 +1278,11 @@ static int process_auth_form_cb(void *_vpninfo,
 		free(password);
 		password = NULL;
 	}
+
+	/* prevent infinite loops if the authgroup requires certificate auth only */
+	if (last_form_empty && empty)
+		return OC_FORM_RESULT_CANCELLED;
+	last_form_empty = empty;
 
 	return OC_FORM_RESULT_OK;
 
